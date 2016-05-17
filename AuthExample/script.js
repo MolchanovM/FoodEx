7ca02c489639412492141af3651c2021413802a3
Oleksii Kholovchuk <vortexxx192@gmail.com>
2016-05-18 00:52:15 +0300

    Able to get data of a specific worksheet.

--- AuthExample/script.js
+++ AuthExample/script.js
@@ -1,8 +1,14 @@
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
   console.log(token);
 
-  var httpStr = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full?access_token=' + token;
-  $.get(httpStr, function(data) {
-  	console.log(data);
+  var httpStr = 'https://spreadsheets.google.com/feeds/worksheets/1Ir0bSQ91yYrfAeYcZVQK3EJSvZkjtOcpMkb_BwY4AXE/private/full?access_token=' + token;
+  $.ajax({
+  	url: httpStr,
+  	type: 'GET',
+  	dataType: 'jsonp',
+  	cache: false,
+  	success: function(response) {
+  		console.log(response);
+  	}
   });
 });
