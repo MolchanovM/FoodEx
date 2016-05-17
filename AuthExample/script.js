651d4bc7583d28f89658f4917fa0d6036f1c430d
Oleksii Kholovchuk <vortexxx192@gmail.com>
2016-05-18 00:13:37 +0300

    Getting list of all spreadsheets.

--- AuthExample/script.js
+++ AuthExample/script.js
@@ -1,3 +1,8 @@
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
   console.log(token);
+
+  var httpStr = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full?access_token=' + token;
+  $.get(httpStr, function(data) {
+  	console.log(data);
+  });
 });
