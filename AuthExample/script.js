b82e9b180319d63301e53bdafd213054cc8c2e49
Oleksii Kholovchuk <vortexxx192@gmail.com>
2016-05-18 01:24:15 +0300

    Able to retrieve data of a specific worksheet.

--- AuthExample/script.js
+++ AuthExample/script.js
@@ -1,7 +1,7 @@
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
   console.log(token);
 
-  var httpStr = 'https://spreadsheets.google.com/feeds/worksheets/1Ir0bSQ91yYrfAeYcZVQK3EJSvZkjtOcpMkb_BwY4AXE/private/full?access_token=' + token;
+  var httpStr = 'https://spreadsheets.google.com/feeds/cells/1Ir0bSQ91yYrfAeYcZVQK3EJSvZkjtOcpMkb_BwY4AXE/omxooju/private/full?access_token=' + token;
   $.ajax({
   	url: httpStr,
   	type: 'GET',
